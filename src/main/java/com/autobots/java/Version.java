8f96564eb7e781b6ecd20e5f2f819143caf59178
nuta312 <musakunovnur@gmail.com>
2025-07-07 11:33:34 +0600

    version 1.0.0

--- src/main/java/com/autobots/java/Version.java
+++ src/main/java/com/autobots/java/Version.java
@@ -5,5 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Version {
     public static void main(String[] args) {
         System.out.println("Version 1.0.0");
+        System.out.println("Version 1.0.1");
     }
 }
