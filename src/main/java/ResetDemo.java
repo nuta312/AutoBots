a9ca79706eb010f1b688ba9dcfdf29a34effea7c
nuta312 <musakunovnur@gmail.com>
2025-07-08 09:23:28 +0600

    Version 1.0.2

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -3,5 +3,6 @@ public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
+        System.out.println("Version 1.0.2");
     }
 }
